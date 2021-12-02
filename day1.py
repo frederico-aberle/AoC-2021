8d59c09101b5c7d054e150ae8161b23bb4141bcc
frederico-aberle <aberle.frederico@gmail.com>
2021-12-02 16:08:04 +0100

    ...

--- day1.py
+++ day1.py
@@ -1,14 +1,17 @@
 file = open("input1.txt").read().splitlines()
 file = [int(x) for x in file]
+
+"""Part One"""
 counter = 0
 for i in range(1, len(file)):
     if file[i] - file[i-1] > 0:
         counter += 1
 print(counter)
 
+"""Part Two"""
 temp = []
 for i in range(len(file)-2):
-    temp.append(file[i] + file[i+1] + file[i+2])
+    temp.append(sum(file[i:i+3]))
 counter = 0
 for i in range(1, len(temp)):
     if temp[i] - temp[i-1] > 0:
--- day2.py
+++ day2.py
@@ -1,5 +1,19 @@
 file = open("input2.txt").read().splitlines()
 
+"""Part One"""
+horizontal = 0
+depth = 0
+for i in range(len(file)):
+    temp = file[i].split()
+    if temp[0] == 'forward':
+        horizontal += int(temp[1])
+    elif temp[0] == 'down':
+        depth += int(temp[1])
+    elif temp[0] == 'up':
+        depth -= int(temp[1])
+print(horizontal*depth)
+
+"""Part Two"""
 horizontal = 0
 depth = 0
 aim = 0
@@ -8,9 +22,8 @@ for i in range(len(file)):
     if temp[0] == 'forward':
         horizontal += int(temp[1])
         depth += aim * int(temp[1])
-    elif temp[0] == 'up':
-        aim -= int(temp[1])
     elif temp[0] == 'down':
         aim += int(temp[1])
-
+    elif temp[0] == 'up':
+        aim -= int(temp[1])
 print(horizontal*depth)
