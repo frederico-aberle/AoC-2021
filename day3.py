5a1adf671749f6cb36be6ce1ced8ca82c449fbdc
frederico-aberle <aberle.frederico@gmail.com>
2021-12-04 10:36:42 +0100

    ...

---
+++ day3.py
@@ -0,0 +1,72 @@
+file = open("input3.txt").read().splitlines()
+
+"""Part One"""
+gamma = ""
+epsilon = ""
+for j in range(len(file[0])):
+    counter = 0
+    for i in range(len(file)):
+        if file[i][j] == "0":
+            counter += 1
+    if counter >= len(file)-counter:
+        gamma += "0"
+        epsilon += "1"
+    else:
+        gamma += "1"
+        epsilon += "0"
+
+def binToDec(bin):
+    dec = 0
+    exp = 0
+    for i in range(len(bin)-1, -1, -1):
+        if int(bin[i]) == 1:
+            dec += 2 ** exp
+        exp += 1
+    return dec
+print(binToDec(gamma) * binToDec(epsilon))
+
+"""Part Two"""
+oxygen = file
+for j in range(len(oxygen[0])):
+    counter = 0
+    for i in range(len(oxygen)):
+        if oxygen[i][j] == "0":
+            counter += 1
+    if counter > len(oxygen)-counter:
+        temp = []
+        for i in range(len(oxygen)):
+            if oxygen[i][j] == "0":
+                temp.append(oxygen[i])
+        oxygen = temp
+    else:
+        temp = []
+        for i in range(len(oxygen)):
+            if oxygen[i][j] == "1":
+                temp.append(oxygen[i])
+        oxygen = temp
+    if len(oxygen) == 1:
+        break
+
+carbondioxide = file
+for j in range(len(carbondioxide[0])):
+    counter = 0
+    for i in range(len(carbondioxide)):
+        if carbondioxide[i][j] == "0":
+            counter += 1
+    if counter <= len(carbondioxide)-counter:
+        temp = []
+        for i in range(len(carbondioxide)):
+            if carbondioxide[i][j] == "0":
+                temp.append(carbondioxide[i])
+        carbondioxide = temp
+    else:
+        temp = []
+        for i in range(len(carbondioxide)):
+            if carbondioxide[i][j] == "1":
+                temp.append(carbondioxide[i])
+        carbondioxide = temp
+    if len(carbondioxide) == 1:
+        break
+print(binToDec(oxygen[0]) * binToDec(carbondioxide[0]))
+
+
---
+++ day4.py
@@ -0,0 +1,103 @@
+file = open("input4.txt")
+
+"""Input"""
+numbers = file.readline().split(",")
+numbers = [int(x) for x in numbers]
+
+file = file.read().splitlines()
+
+for i in range(len(file)):
+    file[i] = file[i].split(" ")
+
+bingos = []
+i = 0
+while i < len(file):
+    if file[i] == ['']:
+        i += 1
+    else:
+        oneBingo = []
+        for j in range(5):
+            temp = []
+            for k in range(len(file[i])):
+                if file[i][k] != '':
+                    temp.append(int(file[i][k]))
+            oneBingo.append(temp)
+            i += 1
+        bingos.append(oneBingo)
+# print(bingos)
+
+def isBingo(marked):
+    # horizontal
+    for i in range(5):
+        if marked[i] == [True for j in range(5)]:
+            return True
+
+    # vertical
+    for j in range(5):
+        bool1 = True
+        for i in range(5):
+            if not marked[i][j]:
+                bool1 = False
+        if bool1:
+            return True
+
+"""Part One"""
+
+index = len(numbers)
+for i in range(len(bingos)):
+    bingo = bingos[i]
+    marked = [[False for i in range(5)] for j in range(5)]
+
+    j = 0
+    while j != len(numbers):
+        for a in range(5):
+            for b in range(5):
+                if bingo[a][b] == numbers[j]:
+                    marked[a][b] = True
+                    break
+        if isBingo(marked):
+            # print(marked)
+            break
+        j += 1
+
+    if j < index:
+        index = j
+        sum = 0
+        for a in range(5):
+            for b in range(5):
+                if not marked[a][b]:
+                    sum += bingo[a][b]
+
+print(sum * numbers[index])
+
+"""Part Two"""
+
+index = 0
+for i in range(len(bingos)):
+    bingo = bingos[i]
+    marked = [[False for i in range(5)] for j in range(5)]
+
+    j = 0
+    while j != len(numbers):
+        for a in range(5):
+            for b in range(5):
+                if bingo[a][b] == numbers[j]:
+                    marked[a][b] = True
+                    break
+        if isBingo(marked):
+            # print(marked)
+            break
+        j += 1
+
+    if j > index:
+        index = j
+        sum = 0
+        for a in range(5):
+            for b in range(5):
+                if not marked[a][b]:
+                    sum += bingo[a][b]
+
+print(sum * numbers[index])
+
+
+
